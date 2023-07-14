da42fc1735949f28104641bad2cd5f1e813ba068
David Lee <Dai180293@gmail.com>
2023-07-14 15:20:09 +0100

    updated readme

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "https://github.com/DLee1993/FEM-results_summary_component"
 })
