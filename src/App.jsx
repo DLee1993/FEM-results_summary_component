8abeaaea325af6afca6986c287b3ef145bf3a4cd
David Lee <Dai180293@gmail.com>
2023-07-14 04:00:48 +0100

    challenge completed

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,98 @@
+import { MemorySVG, VerbalSVG, VisualSVG, ReactionSVG } from "./components/SVG-Components";
 const App = () => {
-    return <section className="bg-blue-600">hello</section>;
+    return (
+        <section
+            id="resultsSummary"
+            className="flex justify-center items-center flex-col sm:flex-row"
+        >
+            <section
+                id="results"
+                className="w-72 h-96 flex justify-evenly items-center flex-col rounded-b-xl sm:rounded-3xl bg-gradient-to-t from-lightRoyalBlue via-lightRoyalBlue to-lightSlateBlue"
+            >
+                <p className="text-paleBlue/70 font-bold">Your Result</p>
+
+                <div
+                    id="circle"
+                    className="w-40 h-40 flex justify-center items-center rounded-full bg-gradient-to-b from-violetBlue to-persianBlue text-white"
+                >
+                    <span
+                        id="nums"
+                        className="flex justify-center items-center flex-col text-5xl font-bold"
+                    >
+                        76
+                        <sub className="mt-2 text-base text-paleBlue/50">of 100</sub>
+                    </span>
+                </div>
+                <p className="flex justify-center items-center flex-col mx-auto text-center max-w-[70%] text-paleBlue/50 font-semibold text-sm">
+                    <span className="mb-4 text-2xl text-white">Great</span>
+                    You scored higher than 67% of the people who have taken these tests.
+                </p>
+            </section>
+            {/**------------------------------------------*/}
+            <section
+                id="summary"
+                className="w-80 h-96 flex justify-center items-center sm:rounded-r-3xl sm:shadow-[30px_25px_20px_-20px_rgba(200,199,255,.4)]"
+            >
+                <section
+                    id="summaryContent"
+                    className="w-full h-full flex justify-evenly items-start flex-col max-w-[90%]"
+                >
+                    <p id="title" className="w-full max-w-[90%] mx-auto text-left font-extrabold">
+                        Summary
+                    </p>
+                    <ul className="w-full max-w-[90%] mx-auto">
+                        <li className="w-full h-10 rounded bg-lightRed/5 text-base py-6 px-2 flex justify-start items-center mb-2">
+                            <ReactionSVG />
+                            <p id="title" className="ml-2 font-semibold text-lightRed/75">
+                                Reaction
+                            </p>
+                            <span id="rating" className="ml-auto">
+                                <span className="font-bold">80</span>
+                                <span className="text-darkGrayBlue/50 mx-1">/</span>
+                                <span className="text-darkGrayBlue/50 font-semibold">100</span>
+                            </span>
+                        </li>
+                        <li className="w-full h-10 rounded bg-orangeYellow/10 text-base py-6 px-2 flex justify-start items-center mb-2">
+                            <MemorySVG />
+                            <p id="title" className="ml-2 font-semibold text-orangeYellow/75">
+                                Memory
+                            </p>
+                            <span id="rating" className="ml-auto">
+                                <span className="font-bold">92</span>
+                                <span className="text-darkGrayBlue/50 mx-1">/</span>
+                                <span className="text-darkGrayBlue/50 font-semibold">100</span>
+                            </span>
+                        </li>
+                        <li className="w-full h-10 rounded bg-greenTeal/10 text-base py-6 px-2 flex justify-start items-center mb-2">
+                            <VerbalSVG />
+                            <p id="title" className="ml-2 font-semibold text-greenTeal/75">
+                                Verbal
+                            </p>
+                            <span id="rating" className="ml-auto">
+                                <span className="font-bold">61</span>
+                                <span className="text-darkGrayBlue/50 mx-1">/</span>
+                                <span className="text-darkGrayBlue/50 font-semibold">100</span>
+                            </span>
+                        </li>
+                        <li className="w-full h-10 rounded bg-cobaltBlue/10 text-base py-6 px-2 flex justify-start items-center mb-2">
+                            <VisualSVG />
+                            <p id="title" className="ml-2 font-semibold text-cobaltBlue/75">
+                                Visual
+                            </p>
+                            <span id="rating" className="ml-auto">
+                                <span className="font-bold">72</span>
+                                <span className="text-darkGrayBlue/50 mx-1">/</span>
+                                <span className="text-darkGrayBlue/50 font-semibold">100</span>
+                            </span>
+                        </li>
+                    </ul>
+                    <button className="w-[90%] mx-auto h-12 rounded-full bg-darkGrayBlue text-white/75 hover:bg-gradient-to-b from-violetBlue to-lightSlateBlue hover:text-white transition-all duration-200">
+                        Continue
+                    </button>
+                </section>
+            </section>
+        </section>
+    );
 };
 
 export default App;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,24 @@
 export default {
     content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
     theme: {
-        fontFamily: {
-            // Add your custom fonts and enjoy.
-            "Hanken-Grotesk": ["Hanken Grotesk", "Sans-serif"],
+        extend: {
+            fontFamily: {
+                "Hanken-Grotesk": ["Hanken Grotesk", "Sans-serif"],
+            },
+            colors: {
+                lightRed: "hsl(0, 100%, 67%)",
+                orangeYellow: "hsl(39, 100%, 56%)",
+                greenTeal: "hsl(166, 100%, 37%)",
+                cobaltBlue: "hsl(234, 85%, 45%)",
+                lightSlateBlue: "hsl(252, 100%, 67%)",
+                lightRoyalBlue: "hsl(241, 81%, 54%)",
+                violetBlue: "hsla(256, 72%, 46%, 1)",
+                persianBlue: "hsla(241, 72%, 46%, 0)",
+                darkGrayBlue: "hsl(224, 30%, 27%)",
+                paleBlue: "hsla(221, 100%, 96%, 0.75)",
+                lightLavender: "hsl(241, 100%, 89%)"
+            },
         },
-        extend: {},
     },
     plugins: [],
 };
